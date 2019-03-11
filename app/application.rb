e6dea42beecc338c001a5bc886efcdb5120e52c0
ytsuji27 <ytsuji91@gmail.com>
2019-03-11 09:12:25 -0700

    Done.

---
+++ app/application.rb
@@ -0,0 +1,32 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+end
+
+
+#
+# /items/<ITEM NAME> #<~~ return the price of item
+# everything else should be 404
+#
+# IF a user requests an item that you don't have, then return a 400 and an error message
